97f915ed0278a447aede95b7fca50c2e1e9a83c7
Guangyu Zhang <zguangyu123@gmail.com>
2014-01-14 03:21:44 +0800

    fix the syncing time

--- genindex.py
+++ genindex.py
@@ -1,39 +1,71 @@
 #!/usr/bin/python
 
 import os, time, os.path
+from get-mirror-status import *
+import json
 
-h=open('/home/mirror/newindex/header.html','r')
+homedir='/home/mirror/'
+basedir='/home/mirror/newindex/' # must has a '/' in the end
+serverdir = '/srv/www/'
+
+h=open(basedir+'header.html','r')
 output=h.read()
 h.close()
 
-dir = '/srv/www/'
+mirrors = open(homedir+'etc/mirrors.json')
+repo_list = json.load(mirrors)
+
+def getRepoInfo(repo_name) :
+        for repo in repo_list:
+                if repo['log_path'] == repo_name:
+                        return repo
+
+        return None
 
 output+='<table cellpadding="0" cellspacing="0" class="filelist">\n'
 output+='<thead><tr id="firstline"><th id="name">Folder</th><th>Last Update</th><th id="help">Help</th></tr></thead>\n'
-for file in sorted(os.listdir(dir)):
-	if file[0] != '.' and not file.endswith('.html') and file != 'tmpfs':
-                logdir=('/home/mirror/log/'+file).lower()
-                if file == 'sourceware.org': logdir='/home/mirror/log/cygwin/'
-                if file == 'scientificlinux': logdir='/home/mirror/log/scientific/'
-                if file == 'progress-linux': logdir='/home/mirror/log/progress/'
-                if file == 'uksm-kernel': logdir='/home/mirror/log/uksm/'
-                if file == 'fedora': logdir='/home/mirror/log/fedora-linux/'
-                if file == 'kde-applicationdata': logdir='/home/mirror/log/kde-application/'
-                try:
-                    modtime=time.strftime('%Y-%m-%d %H:%I:%S', time.localtime(os.path.getmtime(logdir)))
-                except os.error:
-                    modtime=time.strftime('%Y-%m-%d %H:%I:%S', time.localtime(os.path.getmtime(dir+file)))
-		output+='<tr><td class="filename"><a href="/'+file+'">'+file+'</a></td><td class="filetime">'+modtime+'</td><td class="help"><a href="help/'+file+'">Help</a></td></tr>\n'
+for file in sorted(os.listdir(serverdir)):
+        if file[0] != '.' and not file.endswith('.html') and file != 'tmpfs':
+                _file = file    # a wrapper for the real log file name
+                if file == 'sourceware.org':
+                        _file = 'cygwinports'
+                if file == 'scientificlinux':
+                        _file = 'scientific'
+                if file == 'progress-linux':
+                        _file = 'progress'
+                if file == 'uksm-kernel':
+                        _file = 'uksm'
+                if file == 'fedora':
+                        _file = 'fedora-linux'
+                if file == 'kde-applicationdata':
+                        _file = 'kde-application'
+                logdir=(homedir+'log/'+_file).lower()
+                # try:
+                #         modtime=time.strftime('%Y-%m-%d %H:%I:%S', time.localtime(os.path.getmtime(logdir)))
+                # except os.error:
+                #         modtime=time.strftime('%Y-%m-%d %H:%I:%S', time.localtime(os.path.getmtime(dir+file)))
+                repo_info = getRepoInfo(_file)
+                if repo_info is not None:
+                        log_filename = getLogFileName(repo_info['log_path'], repo_info['script_type'])
+                        status = getSyncStatus(log_filename)
+                        if "sync_time" in repo.keys():
+                                modtime = repo["sync_time"]
+                        else:
+                                modtime = getArchiveSyncTime(log_filename, status)
+                else:
+                        modtime = 'Not Syncing'
+                output+='<tr><td class="filename"><a href="/'+file+'">'+file+'</a></td><td class="filetime">'+modtime+'</td><td class="help"><a href="help/'+file+'">Help</a></td></tr>\n'
+
 output+='</table>'
-m=open('/home/mirror/newindex/middle.html','r')
+m=open(basedir+'middle.html','r')
 output+=m.read()
-m.close
-info=open('/home/mirror/newindex/isoinfo.json','r')
+m.close()
+info=open(basedir+'isoinfo.json','r')
 output+=info.read()
-info.close
-f=open('/home/mirror/newindex/footer.html','r')
+info.close()
+f=open(basedir+'footer.html','r')
 output+=f.read()
 f.close()
-w=open('/srv/www/index.html','w')
+w=open(serverdir+'index.html','w')
 w.write(output)
 w.close()
