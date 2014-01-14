0baa36cd7c510980e2ae8fb4a8a75a3acfe4cc2d
Guangyu Zhang <zguangyu123@gmail.com>
2014-01-14 08:15:35 +0800

    fix bioc name

--- genindex.py
+++ genindex.py
@@ -39,6 +39,8 @@ for file in sorted(os.listdir(serverdir)):
                         _file = 'fedora-linux'
                 if file == 'kde-applicationdata':
                         _file = 'kde-application'
+                if file == 'bioc':
+                        _file = 'bioc_2_13'
                 logdir=(homedir+'log/'+_file).lower()
                 # try:
                 #         modtime=time.strftime('%Y-%m-%d %H:%I:%S', time.localtime(os.path.getmtime(logdir)))
