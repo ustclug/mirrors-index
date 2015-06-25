cb9229a7bb66c4f41a16a9070fc925dd07e65b5a
Knight <Knight42@mail.ustc.edu.cn>
2015-06-25 19:17:42 +0800

    genrate static html file with jinja2

---
+++ gencontent.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3 -O
+# -*- coding: utf-8 -*-
+
+import os
+import time
+from urllib import parse as urlparse
+import requests
+import fnmatch
+
+BASEDIR = os.path.dirname(__file__)
+HTTPDIR = '/srv/www'
+
+# Directories match these glob will be ignored.
+EXCLUDE = ("tmpfs", ".*")
+
+# A unreliable workaround for nested repos. See comments in main program.
+def CTimeWA (dirpath):
+    ctime = 0
+    for subd in os.listdir(dirpath):
+        subdirpath = os.path.join(dirpath, subd)
+        if not os.path.isdir(subdirpath):
+            continue
+
+        _ctime = os.stat(subdirpath).st_ctime
+        if _ctime > ctime:
+            ctime = _ctime
+
+    return ctime
+
+
+def testHelpLink (name):
+    URLBASE = "https://lug.ustc.edu.cn/wiki/mirrors/help/"
+    url = urlparse.urljoin(URLBASE, name)
+
+    try:
+        html = requests.get(url, timeout = 4)
+    #except (urllib.error.URLError, urllib.error.HTTPError):
+    except:
+        return False
+
+    return False if "该主题尚不存在" in html.text else True
+
+def genRepoList():
+    now = time.time()
+    for d in sorted(os.listdir(HTTPDIR), key = lambda s: s.lower()):
+        fpath = os.path.join(HTTPDIR, d)
+
+        if not os.path.isdir(fpath) or \
+                any(fnmatch.fnmatch(d, p) for p in EXCLUDE):
+            continue
+
+        # Change time is lastsync time if sync destination is exactly the same
+        # top-level dir of http. However, some repos are divided to several
+        # sub-dirs which are actually sync-ed instead of top-level directory.
+        # We need to check these sub-dirs to find correct lastsync time.
+        ctime = os.stat(fpath).st_ctime
+
+        # Since checking all sub-dirs wastes much time, the script just check
+        # repos whose top-level dirs have a old change time.
+        if time.time() - ctime > 3600*24*2:
+            _ctime = CTimeWA(fpath)
+            if _ctime > ctime:
+                ctime = _ctime
+
+        yield {'DIR':d, 
+                'MODTIME':time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(ctime)), 
+                'HELP':"Help" if testHelpLink(d) else "" }
+
+if __name__ == '__main__':
+    for i in genRepoList():
+        print(i)
