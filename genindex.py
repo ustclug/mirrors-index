c631b0ac9b24cd8bdf50b3a807f95b8520ed4448
Boyuan Yang <073plan@gmail.com>
2016-05-02 18:46:52 +0800

    work of testing purpose

--- genindex.py
+++ genindex.py
@@ -2,22 +2,38 @@
 # -*- coding: utf-8 -*-
 
 import os
+import logging
+import argparse
 from jinja2 import Environment, FileSystemLoader
 import gencontent
 import genisolist
 import genservernews
 
-OUTFILE = os.path.join(gencontent.HTTPDIR, 'index.html')
-BASEDIR = os.path.dirname(__file__)
-env = Environment(loader=FileSystemLoader(os.path.join(BASEDIR, 'templates')))
-template = env.get_template('index.html')
-parsed_template = template.render(
-        repolist=gencontent.genRepoList(),
-        revproxy=gencontent.getOthers(),
-        isoinfo=genisolist.getImageList(),
-        newslist=genservernews.getServerNews())
+def main():
+    parser = argparse.ArgumentParser(
+            description="USTC Mirrors Index Page Generator",
+            epilog="Brought to you by LUG@USTC.")
+    parser.add_argument(
+            '-o', '--output',
+            type=str,
+            default=os.path.join(gencontent.HTTPDIR, 'index.html'),
+            help='specify output html file',
+            )
+    args = parser.parse_args()
+    OUTFILE = args.output
+    BASEDIR = os.path.dirname(__file__)
+    env = Environment(loader=FileSystemLoader(os.path.join(BASEDIR, 'templates')))
+    template = env.get_template('index.html')
+    parsed_template = template.render(
+            repolist=gencontent.genRepoList(),
+            revproxy=gencontent.getOthers(),
+            isoinfo=genisolist.getImageList(),
+            newslist=genservernews.getServerNews())
 
-with open(OUTFILE, 'w') as fout:
-    fout.write(parsed_template)
-    fout.write(os.linesep)
+    with open(OUTFILE, 'w') as fout:
+        fout.write(parsed_template)
+        fout.write(os.linesep)
+
+if __name__ == "__main__":
+    main()
 
