fd31a7e50c6a66439b1e6e5b6ac44ac39deafa23
mirror <mirrors@ustc.edu.cn>
2016-05-04 19:03:29 +0800

    genindex.py: add logging support

--- genindex.py
+++ genindex.py
@@ -10,6 +10,8 @@ import genisolist
 import genservernews
 
 def main():
+    logger = logging.getLogger('mirrors-genindex')
+    ch = logging.StreamHandler()
     OUTDIR = os.getenv('HTTPDIR') or gencontent.HTTPDIR
     parser = argparse.ArgumentParser(
             description="USTC Mirrors Index Page Generator",
@@ -26,21 +28,37 @@ def main():
             default=os.path.join(OUTDIR, 'index.html'),
             help='specify full output file path, and will overwrite outdir option',
             )
+    parser.add_argument(
+            '-v', '--verbose',
+            help='show verbose information',
+            action='store_true',
+            )
     args = parser.parse_args()
+    LOGLEVEL = logging.DEBUG if args.verbose else logging.INFO
+    logger.setLevel(LOGLEVEL)
+    ch.setLevel(LOGLEVEL)
+    ch.setFormatter(logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s'))
+    logger.addHandler(ch)
+    logger.debug('received command arguments: "{}"'.format(str(args)))
     OUTDIR = args.outdir or OUTDIR
     OUTFILE = args.output
     BASEDIR = os.path.dirname(__file__)
+    logger.info('OUTFILE is "%(OUTFILE)s"' % locals())
+    logger.info('BASEDIR is "%(BASEDIR)s"' % locals())
     env = Environment(loader=FileSystemLoader(os.path.join(BASEDIR, 'templates')))
     template = env.get_template('index.html')
+    logger.debug('begin parsing template...')
     parsed_template = template.render(
             repolist=gencontent.genRepoList(),
             revproxy=gencontent.getOthers(),
             isoinfo=genisolist.getImageList(),
             newslist=genservernews.getServerNews())
 
+    logger.info('begin file writing...')
     with open(OUTFILE, 'w') as fout:
         fout.write(parsed_template)
         fout.write(os.linesep)
+    logger.info('completed! exiting...')
 
 if __name__ == "__main__":
     main()
